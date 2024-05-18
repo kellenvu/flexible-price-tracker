5f7ad186b599d21bce1040d103ff04be57d83be7
Kellen Vu <kellen.vo.vu@gmail.com>
2024-05-18 08:23:43 -0700

    Update track_price.py

--- track_price.py
+++ track_price.py
@@ -10,44 +10,49 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-##########
-# CONFIG
-##########
+#--------#
+# CONFIG #
+#--------#
 
 # Required
 EXCEL_FILEPATH = 'items.xlsx'
-SCRIPT_DIRECTORY_ABS_PATH = r''
+SCRIPT_DIRECTORY_ABS_PATH = r'D:\Users\path\to\Price Tracker'
 
 # Booleans
 HIDE_CHROME_UI = False  # Set to True if you want to hide the Chrome UI while it webscrapes (can lead to issues with some sites)
-PRINT_TO_TXT = False  # Set to True if you want the print() statements to print to a txt file instead of the console (useful if you run this on an auto-scheduler)
+PRINT_TO_TXT = True  # Set to True if you want the print() statements to print to a txt file instead of the console (useful if you run this on an auto-scheduler)
 
 # Notion integration
-NOTION_SECRET = ''
-NOTION_DATABASE_ID = ''
+NOTION_SECRET = 'secret_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+NOTION_DATABASE_ID = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
-#######
-# CODE
-#######
+#------#
+# CODE #
+#------#
 
-def alert(items_with_price_drop):
-    """
-    Alerts the user about items with price drops.
+def alert(items_with_price_drop: list[dict]) -> None:
+    """Alerts the user about items with price drops.
 
     This function posts a message to a Notion page using the Notion API.
 
     If you would like to be alerted a different way (e.g. print to console, email), then modify this function.
+
+    Args:
+        items_with_price_drop: List of items have that dropped in price
     """
     message = "Price dropped for the following items:\n"
+
     for item in items_with_price_drop:
         message += f"{item['name']} is now ${item['current_price']:.2f} (was ${item['last_price']:.2f}). Link: {item['url']}\n"
 
     endpoint = "https://api.notion.com/v1/pages"
+
     headers = {
         "Authorization": f"Bearer {NOTION_SECRET}",
         "Content-Type": "application/json",
         "Notion-Version": "2022-06-28"
     }
+
     body = {
         "parent": {
             "database_id": NOTION_DATABASE_ID
@@ -88,12 +93,22 @@ def alert(items_with_price_drop):
 
     if response.status_code == 200:
         print("Message successfully posted to Notion!")
+
     else:
-        print(
-            f"Failed to post message to Notion. Status code: {response.status_code}, Response: {response.text}")
+        print(f"Failed to post message to Notion. Status code: {response.status_code}, Response: {response.text}")
 
 
-def get_price(driver, item):
+def get_price(driver, item) -> float:
+    """Gets the price of the item using the web driver.
+    
+    Args:
+        driver: The web driver
+        item: The item, which must contain a list of comma-delimited CSS selectors
+        
+    Returns:
+        float: The items' price
+    """
+    
     driver.get(item['url'])
 
     # Explicitly wait until the element is present
@@ -102,19 +117,21 @@ def get_price(driver, item):
     )
 
     # Use a regex pattern to match any numeric value including decimal points
-    match = re.search(r'(\d+\.\d+|\d+)', price_element.text)
-    if match and match.group(0):
-        return float(match.group(0))
+    matches = re.findall(r'\d+\.\d+|\d+', price_element.text)
+
+    if matches:
+        return min(float(price) for price in matches)
+    
     else:
-        raise ValueError(f"No price found for {item['name']} at {item['url']}")
+        raise ValueError(f"no price found for {item['name']} at {item['url']}")
 
 
 def get_driver():
+
     # Options to suppress logging
     chrome_options = Options()
     chrome_options.add_argument("--log-level=3")
-    chrome_options.add_experimental_option(
-        'excludeSwitches', ['enable-logging'])
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
     
     if HIDE_CHROME_UI:
         chrome_options.add_argument("--headless")
@@ -123,9 +140,11 @@ def get_driver():
 
 
 def main():
+
     os.chdir(SCRIPT_DIRECTORY_ABS_PATH)
     
     with open('output.txt', 'w') as f:
+
         if PRINT_TO_TXT:
             sys.stdout = f
 
@@ -138,6 +157,7 @@ def main():
 
         # Assuming the data starts on row 2 (row 1 being the header)
         for row in range(2, sheet.max_row + 1):
+
             item = {
                 'name': sheet.cell(row=row, column=1).value,
                 'url': sheet.cell(row=row, column=2).value,
@@ -146,6 +166,7 @@ def main():
             }
 
             try:
+
                 current_price = get_price(driver, item)
                 sheet.cell(row=row, column=4).value = current_price
 
@@ -159,7 +180,7 @@ def main():
                     items_with_price_drop.append(item)
 
             except Exception as e:
-                print(f"Error getting price for {item['name']}: {e}")
+                print(f"Failed to get price for {item['name']}: {e}")
                 continue
 
         workbook.save(EXCEL_FILEPATH)
